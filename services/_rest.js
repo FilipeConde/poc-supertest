ea4d6e5a410285f6fd3cb6a06e711990ca046519
Filipe Conde <filipecondep@gmail.com>
2025-12-03 23:31:59 -0300

    chore: create file to put http methods.

---
+++ services/_rest.js
@@ -0,0 +1,8 @@
+import supertest from 'supertest';
+
+const BASE_URL = 'http://localhost:3000';
+const api = supertest(BASE_URL);
+
+export default class Rest {
+  // put here methods for http requests
+}
