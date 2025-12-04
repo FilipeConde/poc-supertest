848b343402a2caad4f1512c43775ee1047b12248
Filipe Conde <filipecondep@gmail.com>
2025-12-04 00:12:57 -0300

    chore: improve test file.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const assert = require("assert");
+// import Rest from "../services/_rest";
+import { describe, it } from "mocha";
+import assert from "assert";
 
-describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe("Main describe", () => {
+  it("Sample test", function () {
+    assert.equal([1, 2, 3].indexOf(4), -1);
   });
 });
